59c0a95ed391b2ad596878c6def123752d8129e4
mohdashraf010897 <mohdashraf010897@gmail.com>
2020-04-18 11:07:40 +0530

    Updates

---
+++ precache-manifest.5d01cfe9910ef765d4740e840d673cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bb4c35c5e7f2df51ef813c6011970c",
+    "url": "/Monsters-Basic-React/index.html"
+  },
+  {
+    "revision": "8262385a61fb7be4011f",
+    "url": "/Monsters-Basic-React/static/css/main.121024ec.chunk.css"
+  },
+  {
+    "revision": "5abeac6ab14e7b5ba573",
+    "url": "/Monsters-Basic-React/static/js/2.8e4dce56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Basic-React/static/js/2.8e4dce56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8262385a61fb7be4011f",
+    "url": "/Monsters-Basic-React/static/js/main.aa1c0c18.chunk.js"
+  },
+  {
+    "revision": "118f3805b4304f4ff647",
+    "url": "/Monsters-Basic-React/static/js/runtime-main.d0c3e81f.js"
+  }
+]);
